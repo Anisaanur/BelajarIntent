63108c67813c9a397652482f87965c8def9402da
Luthfi Pratama Wendy <pratamaluthfi605@gmail.com>
2023-03-15 10:05:23 +0700

    Belajar Intent

---
+++ app/src/main/java/com/anisa/belajarintent/PindahDataObjek.java
@@ -0,0 +1,10 @@
+package com.anisa.belajarintent;
+
+import android.app.Activity;
+
+import org.jetbrains.annotations.Nullable;
+
+public class PindahDataObjek extends Activity {
+    @Nullable
+    public static final String EXTRA_CARS = new String();
+}
